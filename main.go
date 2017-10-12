0dd271f0d0ac5059d1c4b6f96ee86a790bfc3b9d
Seth Vargo <seth@sethvargo.com>
2017-10-12 17:17:08 -0400

    Fix import path
    How this compiled is beyond me...

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/sethvargo/terraform-provider-google-calendar/googlecalendar"
+	"github.com/sethvargo/terraform-provider-googlecalendar/googlecalendar"
 )
 
 func main() {
